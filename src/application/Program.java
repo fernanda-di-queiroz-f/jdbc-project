10e5df8ce9370ee8268467aae065e2af147ac67d
Fernanda <fernanda1queiroz44@gmail.com>
2020-08-05 15:11:25 -0300

    Created findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(1, "Books");
-		System.out.println(department);
-
-		Seller seller = new Seller(1, "Julia Roberts", "julia@gamil.com", new Date(), 3000.0, department);
-		System.out.println(seller);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
