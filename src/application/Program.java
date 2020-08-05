a190e39c1d4a1facdb83503f4d7677e85011b787
Fernanda <fernanda1queiroz44@gmail.com>
2020-08-05 15:45:29 -0300

    Created findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,11 +20,20 @@ public class Program {
 		System.out.println(seller);
 
 		System.out.println();
-		System.out.println("=== TEST 1 : Seller findByDepartment ===");
+		System.out.println("=== TEST 2 : Seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 
 		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println();
+		System.out.println("=== TEST 3 : Seller findAll ===");
+
+		List<Seller> list1 = sellerDao.findAll();
+
+		for (Seller obj : list1) {
 			System.out.println(obj);
 		}
 
